f0ea3f1ac7912ee5212089c27333893c118a815e
Justin Sorensen <jus.a.sorensen@gmail.com>
2019-05-29 20:28:58 -0500

    Handle malformed/non-JSON payloads for POST's to /user

--- spec/cucumber/steps/index.js
+++ spec/cucumber/steps/index.js
@@ -19,8 +19,6 @@
 import superagent from 'superagent';
 import { When, Then } from 'cucumber';
 /** Node module for specifying expected and actual test results */
-// import { AssertionError } from 'assert';
-/** refactor: let's access other assert features instead */
 import assert from 'assert';
 
 
@@ -38,42 +36,191 @@ import assert from 'assert';
  */
 
 
-/*----------  step definitions  ----------*/
+/*----------  scenarios  ----------*/
+/**
+ * 1. Client sends `POST` to `/users` with empty payload
+ *    API responds with `400 Bad Request` & JSON object with
+ *    error message
+ *
+ * 2. Client sends `POST` to `/users` with non-JSON payload
+ *    API responds with `415 Unsupported Media Type` & JSON
+ *    object with error message
+ *
+ * 3. Client sends `POST` to `/users` with malformed JSON
+ *    API responds with `400 Bad Request` & JSON object with
+ *    error message
+ */
 
+
+/*----------  step definitions  ----------*/
 
 /** Cucumber runs the appropriate definition due to pattern matching */
 
+/** refactor: keep closer to DRY */
+// /**
+//  * start building the request with a new request object...
+//  */
+// When('the client creates a POST request to /users', function () {
+//   /** refactor: use context object instead of global var */
+//   // request = superagent('POST', 'localhost:8080/users');
+//   /** refactor: remove hardcoded vals, use .env */
+//   // this.request = superagent('POST', 'localhost:8080/users');
+//   this.request = superagent(
+//     'POST', `${process.env.SERVER_HOSTNAME}:${process.env.SERVER_PORT}/users`
+//   );
+// });
+//
+// When('attaches a generic empty payload', function () {
+//   /** sending empty payloads is superagent's default behavior */
+//   return undefined;
+// });
+//
+// When('attaches a generic non-JSON payload', function () {
+//   this.request.send(
+//     '<?xml version="1.0" encoding="UTF-8" ?><email>justin@jsore.com</email>'
+//   );
+//   this.request.set('Content-Type', 'text/xml');
+// });
+//
+// When('attaches a generic malformed payload', function () {
+//   this.request.send('{"email": "justin@jsore.com", name: }');
+//   this.request.set('Content-Type', 'application/json');
+// });
+//
+// /**
+//  * ...complete and send request to testing API server with a
+//  * callback specified to keep next step from running early...
+//  */
+// When('sends the request', function (callback) {
+//   //request
+//   this.request
+//     .then((response) => {
+//       /**
+//        * ...and save the response elsewhere then finally hit
+//        * callback() after response received and saved
+//        */
+//       this.response = response.res;
+//       callback();
+//     })
+//     .catch((error) => {
+//       this.response = error.response;
+//       callback();
+//     });
+// });
+//
+// /**
+//  * scenario 1: check method and path of req in requestHandler,
+//  * if match POST & /users, send 400
+//  */
+// /** refactor: use regex to escape special characters */
+// // Then('our API should respond with a 400 HTTP status code', function () {
+// /** refactor: use parameters to keep DRY */
+// // Then(/^our API should respond with a 400 HTTP status code$/, function () {
+// Then(/^our API should respond with a ([1-5]\d{2}) HTTP status code$/, function (statusCode) {
+//   /** refactor: be more concise */
+//   // if (this.response.statusCode !== 400) {
+//   //   /** generic assertion failure */
+//   //   // throw new Error();
+//   //   /** verbose failure messages */
+//   //   throw new AssertionError({
+//   //     expected: 400,
+//   //     actual: this.response.statusCode,
+//   //   });
+//   // }
+//   /** throw AssertionError if paramters are not equal */
+//   //assert.equal(this.response.statusCode, 400);
+//   assert.equal(this.response.statusCode, statusCode);
+// });
+//
+// //Then('our API should respond with a 415 HTTP status code', function () {
+// Then(/^our API should respond with a 415 HTTP status code$/, function () {
+//   assert.equal(this.response.statusCode, 415);
+// });
+//
+// // Then('contains a message property which says \'The "Content-Type" header must always be "application/json"\'', function () {
+// // fails: Gherkin uses /'s as "alternative text" feature, replace with regex
+// Then(/^contains a message property which says 'The "Content-Type" header must always be "application\/json"'$/, function () {
+//   assert.equal(this.responsePayload.message, 'The "Content-Type" header must always be "application/json"');
+// });
+//
+// // Then('contains a message property which says "Payload should be in JSON format"', function () {
+// Then(/^contains a message property which says "Payload should be in JSON format"$/, function () {
+//   assert.equal(this.responsePayload.message, 'Payload should be in JSON format');
+// });
+//
+// /**
+//  * scenario 2: client must send valid JSON object
+//  */
+// // Then('the payload of the response should be a JSON object', function () {
+// Then(/^the payload of the response should be a JSON object$/, function () {
+//   const response = result || error;
+//   /** review body and content-type header for JSON */
+//   const contentType = this.response.headers['Content-Type'] || this.response.headers['content-type'];
+//   if (!contentType || !contentType.includes('application/json')) {
+//     /** generic assertion failure */
+//     throw new Error('Response not of Content-Type application/json');
+//   }
+//   /** review validity of the JSON if JSON received */
+//   try {
+//     //payload = JSON.parse(response.text);
+//     this.responsePayload = JSON.parse(this.response.text);
+//   } catch (e) {
+//     /** generic assertion failure */
+//     throw new Error('Response not a valid JSON object');
+//   }
+// });
+//
+// /**
+//  * scenario 3: error object must contain property of message
+//  * with appropriate message text
+//  */
+// // Then('contains a message property which says "Payload should not be empty"', function () {
+// Then(/^contains a message property which says "Payload should not be empty"$/, function () {
+//   // if (payload.message !== 'Payload should not be empty') {
+//   /** refactor: be more concise, use assert.equal */
+//   // if (this.responsePayload.message !== 'Payload should not be empty') {
+//   //   /** generic assertion failure */
+//   //   throw new Error();
+//   // }
+//   assert.equal(this.responsePayload.message, 'Payload should not be empty');
+// });
 
-/**
- * start building the request with a new request object...
- */
-When('the client creates a POST request to /users', function () {
-  /** refactor: use context object instead of global var */
-  // request = superagent('POST', 'localhost:8080/users');
-  /** refactor: remove hardcoded vals, use .env */
-  // this.request = superagent('POST', 'localhost:8080/users');
-  this.request = superagent(
-    'POST', `${process.env.SERVER_HOSTNAME}:${process.env.SERVER_PORT}/users`
-  );
+
+const host = process.env.SERVER_HOSTNAME;
+const port = process.env.SERVER_PORT;
+const httpReq = /^the client creates a (GET|POST|PATCH|PUT|DELETE|OPTIONS|HEAD) request to ([/\w-:.]+)$/;
+
+
+/** start a new request with a new request object */
+When (httpReq, function(method, path) {
+  this.request = superagent(method, `${host}:${port}${path}`);
 });
 
-When('attaches a generic empty payload', function () {
-  /** sending empty payloads is superagent's default behavior */
-  return undefined;
+
+/** case values pulled from scenario outline's datatable */
+When(/^attaches a generic (.+) payload$/, function (payloadType) {
+  switch (payloadType) {
+    case 'malformed':
+      this.request
+        .send('{"email": "justin@jsore.com", name: }')
+        .set('Content-Type', 'application/json');
+      break;
+    case 'non-JSON':
+      this.request
+        .send('<?xml version="1.0" encoding="UTF-8" ?><email>justin@jsore.com</email>')
+        .set('Content-Type', 'text/html');
+      break;
+    case 'empty':
+    default:
+  }
 });
 
-/**
- * ...complete and send request to testing API server with a
- * callback specified to keep next step from running early...
- */
-When('sends the request', function (callback) {
-  //request
+
+When(/^sends the request$/, function(callback) {
+  /** wait until response received... */
   this.request
     .then((response) => {
-      /**
-       * ...and save the response elsewhere then finally hit
-       * callback() after response received and saved
-       */
+      /** ...then store it */
       this.response = response.res;
       callback();
     })
@@ -84,59 +231,29 @@ When('sends the request', function (callback) {
 });
 
 
-
-/**
- * to pass: check method and path of req in requestHandler,
- * if match POST & /users, send 400
- */
-Then('our API should respond with a 400 HTTP status code', function () {
-  /** refactor: be more concise */
-  // if (this.response.statusCode !== 400) {
-  //   /** generic assertion failure */
-  //   // throw new Error();
-  //   /** verbose failure messages */
-  //   throw new AssertionError({
-  //     expected: 400,
-  //     actual: this.response.statusCode,
-  //   });
-  // }
-  /** throw AssertionError if paramters are not equal */
-  assert.equal(this.response.statusCode, 400);
+Then(/^our API should respond with a ([1-5]\d{2}) HTTP status code$/, function (statusCode) {
+  assert.equal(this.response.statusCode, statusCode);
 });
 
-/**
- * to pass: client must send valid JSON object
- */
-Then('the payload of the response should be a JSON object', function () {
-  const response = result || error;
 
-  /** review body and content-type header for JSON */
-  const contentType = this.response.headers['Content-Type'] || this.response.headers['content-type'];
+Then(/^the payload of the response should be a JSON object$/, function () {
+  /** parse header to verify we're sending back JSON */
+  const contentType = this.response.headers['Content-Type'] ||
+    this.response.headers['content-type'];
   if (!contentType || !contentType.includes('application/json')) {
-    /** generic assertion failure */
     throw new Error('Response not of Content-Type application/json');
   }
 
-  /** review validity of the JSON if JSON received */
+  /** check for valid JSON */
   try {
-    //payload = JSON.parse(response.text);
+    /** throws error if .parse() fails */
     this.responsePayload = JSON.parse(this.response.text);
   } catch (e) {
-    /** generic assertion failure */
     throw new Error('Response not a valid JSON object');
   }
 });
 
-/**
- * to pass: error object must contain property of message
- * with appropriate message text
- */
-Then('contains a message property which says "Payload should not be empty"', function () {
-  // if (payload.message !== 'Payload should not be empty') {
-  /** refactor: be more concise */
-  // if (this.responsePayload.message !== 'Payload should not be empty') {
-  //   /** generic assertion failure */
-  //   throw new Error();
-  // }
-  assert.equal(this.responsePayload.message, 'Payload should not be empty');
+
+Then(/^contains a mesage property which says (?:"|')(.*)(?:"|')$/, function (message) {
+  assert.equal(this.responsePayload.message, message);
 });
--- src/index.js
+++ src/index.js
@@ -26,21 +26,70 @@ import http from 'http';
 
 const requestHandler = function (req, res) {
   /** block clients from sending POST's to /users endpoint */
+  // if (req.method === 'POST' && req.url === '/users') {
+  //   /** refactor: send back a valid JSON object in payload */
+  //   // res.statusCode = 400;
+  //   // res.end();
+  //   res.writeHead(400, { 'Content-Type': 'application/json' });
+  //   /** this is the error object */
+  //   res.end(JSON.stringify({
+  //     message: 'Payload should not be empty',
+  //   }));
+  //   return;
+  // }
   if (req.method === 'POST' && req.url === '/users') {
-    /** refactor: send back a valid JSON object in payload */
-    // res.statusCode = 400;
-    // res.end();
-    res.writeHead(400, { 'Content-Type': 'application/json' });
+    const payloadData = [];
+    /**
+     * bodies can be big, use req's ReadleStream interface to
+     * grab each data chunk as it arrives
+     */
+    req.on('data', (data) => {
+      payloadData.push(data);
+    });
 
-    /** this is the error object */
-    res.end(JSON.stringify({
-      message: 'Payload should not be empty',
-    }));
-    return;
+    /** wait until req's data stream is done ( 'end' event ) */
+    req.on('end', () => {
+      /** handle a client's Empty Payload */
+      if (payloadData.length === 0) {
+        res.writeHead(400, { 'Content-Type': 'application/json' });
+        /** send JSON object back to client */
+        res.end(JSON.stringify({
+          message: 'Payload should not be empty',
+        }));
+        return;
+      }
+      /** handle payloads using Unsupported Media Type */
+      if (req.headers['content-type'] !== 'application/json') {
+        res.writeHead(415, { 'Content-Type': 'application/json' });
+        /** send JSON object back to client */
+        res.end(JSON.stringify({
+          message: 'The "Content-Type" header must always be "application/json"',
+        }));
+        return;
+      }
+      /** try to parse the req, looking for malformed JSON */
+      try {
+        /**
+         * Buffer is the param passed into event listener for
+         * data events, representing a chunk of raw data
+         */
+        const bodyString = Buffer.concat(payloadData).toString();
+        /** each chunk should be JSON, this fails if its not */
+        JSON.parse(bodyString);
+      } catch (e) {
+        /** catch any JSON.parse SyntaxError's ( malformed JSON ) */
+        res.writeHead(400, { 'Content-Type': 'application/json' });
+        /** send JSON object back to client */
+        res.end(JSON.stringify({
+          message: 'Payload should be in JSON format',
+        }));
+      }
+    });
+  } else {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    /** finsih prepping response and send to client */
+    res.end('I know a UDP joke, but you might not get it.');
   }
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  /** finsih prepping response and send to client */
-  res.end('I know a UDP joke, but you might not get it.');
 };
 
 const server = http.createServer(requestHandler);
