cfb41888833e2ba5834b5ca9f2007cbe4c8fc899
Justin Sorensen <jus.a.sorensen@gmail.com>
2019-06-11 12:17:20 -0500

    Add success scenario 1

--- spec/cucumber/steps/index.js
+++ spec/cucumber/steps/index.js
@@ -56,9 +56,9 @@ import { getValidPayload, convertStringToArray } from './utils';
 const host = process.env.SERVER_HOSTNAME;
 const port = process.env.SERVER_PORT;
 const createUserWhenHTTP = /^the client creates a (GET|POST|PATCH|PUT|DELETE|OPTIONS|HEAD) request to ([/\w-:.]+)$/;
-const createUserCaseXML = '<?xml version="1.0" encoding="UTF-8" ?><email>justin@jsore.com</email>';
+// const createUserCaseXML = '<?xml version="1.0" encoding="UTF-8" ?><email>justin@jsore.com</email>';
 const createUserFirstThen = /^our API should respond with a ([1-5]\d{2}) HTTP status code$/;
-const createUserSecondThen = /^the payload of the response should be a JSON object$/;
+// const createUserSecondThen = /^the payload of the response should be a JSON object$/;
 const createUserThirdThen = /^contains a message property which says (?:"|')(.*)(?:"|')$/;
 const createUserWhenMissingField = /^attaches an? (.+) payload which is missing the ([a-zA-Z0-9, ]+) fields?$/;
 const createUserNotTypeStr = /^attaches an? (.+) payload where the ([a-zA-Z0-9, ]+) fields? (?:is|are)(\s+not)? a ([a-zA-Z]+)$/;
@@ -82,20 +82,31 @@ When(/^attaches a generic (.+) payload$/, function (payloadType) {
       this.request
         .send('{"email": "justin@jsore.com", name: }')
         .set('Content-Type', 'application/json');
-      break;
+      // break;
+      return;
     case 'non-JSON':
       this.request
-        .send(`${createUserCaseXML}`)
-        //.set('Content-Type', 'text/html');
+        // .send(`${createUserCaseXML}`)
+        .send('<?xml version="1.0" encoding="UTF-8" ?><email>justin@jsore.com</email>')
+        // .set('Content-Type', 'text/html');
         .set('Content-Type', 'text/xml');
-      break;
+      // break;
+      return;
     /** superagent sends a blank payload by default, use it */
     case 'empty':
     default:
+    return;
   }
 });
 
 // attaches a valid payload
+When(/^attaches a valid (.+) payload$/, function (payloadType) {
+  this.requestPayload = getValidPayload(payloadType);
+  this.request
+    .send(JSON.stringify(this.requestPayload))
+    .set('Content-Type', 'application/json')
+  // return;
+});
 
 /**
  * ...And attaches a payload with missing <missingFields> field
@@ -140,17 +151,27 @@ When(createUserNotTypeStr, function (payloadType, fields, invert, type) {  // re
     .set('Content-Type', 'application/json');
 });
 
+// When(/^attaches an? (.+) payload where the ([a-zA-Z0-9, ]+) fields? (?:is|are) exactly (.+)$/, function (payloadType, fields, value) {
+//   const payload = {
+//     email: 'e@ma.il',
+//     password: 'password',
+//   };
+//   const fieldsToModify = fields.split(',').map(s => s.trim()).filter(s => s !== '');
+//   fieldsToModify.forEach((field) => {
+//     payload[field] = value;
+//   });
+//   this.request
+//     .send(JSON.stringify(payload))
+//     .set('Content-Type', 'application/json');
+// });
 When(/^attaches an? (.+) payload where the ([a-zA-Z0-9, ]+) fields? (?:is|are) exactly (.+)$/, function (payloadType, fields, value) {
-  const payload = {
-    email: 'e@ma.il',
-    password: 'password',
-  };
-  const fieldsToModify = fields.split(',').map(s => s.trim()).filter(s => s !== '');
+  this.requestPayload = getValidPayload(payloadType);
+  const fieldsToModify = convertStringToArray(fields);
   fieldsToModify.forEach((field) => {
-    payload[field] = value;
+    this.requestPayload[field] = value;
   });
   this.request
-    .send(JSON.stringify(payload))
+    .send(JSON.stringify(this.requestPayload))
     .set('Content-Type', 'application/json');
 });
 
@@ -189,18 +210,45 @@ Then(createUserFirstThen, function (statusCode) {
 /**
  * ...And the payload of the response should be a JSON object...
  */
-Then(createUserSecondThen, function () {
-  /** parse header to verify we're sending back JSON */
-  const contentType = this.response.headers['Content-Type'] ||
-    this.response.headers['content-type'];
-  if (!contentType || !contentType.includes('application/json')) {
-    throw new Error('Response not of Content-Type application/json');
+ /** refactor: make it more generic */
+// Then(createUserSecondThen, function () {
+//   /** parse header to verify we're sending back JSON */
+//   const contentType = this.response.headers['Content-Type'] ||
+//     this.response.headers['content-type'];
+//   if (!contentType || !contentType.includes('application/json')) {
+//     throw new Error('Response not of Content-Type application/json');
+//   }
+//   try {
+//     /** check for valid JSON, throws error if .parse() fails */
+//     this.responsePayload = JSON.parse(this.response.text);
+//   } catch (e) {
+//     throw new Error('Response not a valid JSON object');
+//   }
+// });
+Then(/^the payload of the response should be an? ([a-zA-Z0-9, ]+)$/, function (payloadType) {
+  const contentType = this.response.headers['Content-Type'] || this.response.headers['content-type'];
+  if (payloadType === 'JSON object') {
+    /** verify header */
+    if (!contentType || !contentType.includes('application/json')) {
+      throw new Error('Response not of Content-Type application/json');
+    }
+    /** make sure it's valid JSON */
+    try {
+      this.responsePayload = JSON.parse(this.response.text);
+    } catch (e) {
+      throw new Error('Response not a valid JSON object');
+    }
   }
-  try {
-    /** check for valid JSON, throws error if .parse() fails */
-    this.responsePayload = JSON.parse(this.response.text);
-  } catch (e) {
-    throw new Error('Response not a valid JSON object');
+  else if (payloadType === 'string') {
+    /** verify header */
+    if (!contentType || !contentType.includes('text/plain')) {
+      throw new Error('Response not of Content-Type text/plain');
+    }
+    /** make sure it's a string */
+    this.responsePayload = this.response.text;
+    if (typeof this.responsePayload !== 'string') {
+      throw new Error('Response not a string');
+    }
   }
 });
 
