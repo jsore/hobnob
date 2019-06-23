6ea48ef7d96f5c7030afe000e57e0f9672d9a4f0
Justin Sorensen <jus.a.sorensen@gmail.com>
2019-06-23 15:57:30 -0500

    Add unit tests for ValidationError

--- src/validators/errors/validation-error/index.js
+++ src/validators/errors/validation-error/index.js
@@ -1,5 +1,7 @@
 /**
- * hobnob/src/validators/errors/validation-error.js
+ * hobnob/src/validators/errors/validation-error/index.js
+ *
+ * from: hobnob/src/validators/errors/validation-error.js
  *
  * extension of Error object to define a custom error,
  * shared interface between handling requests and request
