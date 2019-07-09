84e423e6ea318b8790ba806bec631a4eee231a8d
Justin Sorensen <jus.a.sorensen@gmail.com>
2019-07-09 15:18:21 -0500

    Begin bootstrapping the integration test for Retrieve Engine

--- src/engines/users/retrieve/index.integration.test.js
+++ src/engines/users/retrieve/index.integration.test.js
@@ -1,3 +1,24 @@
+/* eslint-disable */
 /**
  * src/engines/users/retrieve/index.integration.test.js
  */
+
+
+/**
+ * dont forget to remove the linting disable rule
+ */
+
+
+import assert from 'assert';
+import retrieve from '.';
+import elasticsearch from 'elasticsearch';
+
+const db = new elasticsearch.Client({
+  host: `${process.env.ELASTICSEARCH_HOSTNAME}:${process.env.ELASTICSEARCH_PORT}`,
+});
+
+describe('Engine - Users - Retrieve', function () {
+  beforeEach(function () {
+
+  });
+});
