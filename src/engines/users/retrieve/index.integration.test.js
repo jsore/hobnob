9172dafc201d9405c2a10c7c2f0e688a1c8e4865
Justin Sorensen <jus.a.sorensen@gmail.com>
2019-07-10 14:53:30 -0500

    Backing up bootstrapped version before attempting to hand roll a solution

--- src/engines/users/retrieve/index.integration.test.js
+++ src/engines/users/retrieve/index.integration.test.js
@@ -10,15 +10,39 @@
 
 
 import assert from 'assert';
-import retrieve from '.';
 import elasticsearch from 'elasticsearch';
+import retrieve from '.';
 
 const db = new elasticsearch.Client({
   host: `${process.env.ELASTICSEARCH_HOSTNAME}:${process.env.ELASTICSEARCH_PORT}`,
 });
 
-describe('Engine - Users - Retrieve', function () {
-  beforeEach(function () {
+const USER_ID = 'TEST_USER_ID';
+const RETRIEVE_USER_OBJ = {
+  email: 'e@ma.il',
+};
 
+describe('Engine - Users - Retrieve', function () {
+  const req = {
+    params: {
+      userId: USER_ID,
+    },
+  };
+  let promise;
+
+  describe('When the user does not exist', function () {
+
+  });
+
+  describe('When the user exists', function () {
+    beforeEach(function () {
+
+    });
+    afterEach(function () {
+
+    });
+    describe('When the Elasticsearch operation is successful', function () {
+
+    });
   });
 });
